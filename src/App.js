4535a722273b1138fbec330328d95229c96bd902
pedrosalucas <pedrosa.lucas@hotamil.com>
2021-02-21 22:48:34 -0300

    Add Header :tophat:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/index';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from "styled-components";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import UsuarioSVG  from "../Assets/usuario.svg";
+
+const HeaderContainer = styled.header`
+    box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.15);
+    position: fixed;
+    width: 100%;
+    z-index: 100;
+    background: #fff;
+    top: 0;
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 4rem;
+    }
+`;
+
+const NavLinkLogo = styled(NavLink)`
+    padding: .5rem 0;
+    width: max(3vw, 40px);
+`;
+
+const NavLinkLogin = styled(NavLink)`
+    color: #333;
+    display: flex;
+    align-items: baseline;
+
+    &:after {
+        content: '';
+        display: block;
+        width: 14px;
+        height: 17px;
+        background: url(${UsuarioSVG}) no-repeat center center;
+        margin: .5rem;
+    }
+`;
+
+const Header = () => {
+    return (
+        <HeaderContainer className="container">
+            <nav>
+                <NavLinkLogo to="/" aria-label="Dogs - Home">
+                    <Dogs />
+                </NavLinkLogo>
+                <NavLinkLogin to="/login">Login / Criar</NavLinkLogin>
+            </nav>
+        </HeaderContainer>
+    )
+};
+
+export default Header;
