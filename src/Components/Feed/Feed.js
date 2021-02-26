dc8ae35d85c6ddb94e971439efefffdce7c20e73
pedrosalucas <pedrosa.lucas@hotamil.com>
2021-02-25 22:01:14 -0300

    Infinite Scroll :repeat:

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -2,13 +2,41 @@ import React from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
 
-const Feed = () => {
+const Feed = ({ user }) => {
     const [modalPhoto, setModalPhoto] = React.useState(null);
+    const [pages, setPages] = React.useState([ 1 ]);
+    const [infinite, setInfinite] = React.useState(true);
+
+    React.useEffect(() => {
+        let wait = false;
+        function ifiniteScroll() {
+            if( infinite ) {
+                const scroll = window.scrollY;
+                const height = document.body.offsetHeight - window.innerHeight;
+                if( scroll > height * 0.75 && !wait ) {
+                    setPages(pages => [...pages, pages.length + 1]);
+                    wait = true;
+                    setTimeout(() => {
+                        wait = false;
+                    }, 500);
+                }
+            }
+        }
+
+        window.addEventListener('wheel', ifiniteScroll);
+        window.addEventListener('scroll', ifiniteScroll);
+        return (() => {
+            window.removeEventListener('wheel', ifiniteScroll);
+            window.removeEventListener('scroll', ifiniteScroll);
+        });
+    }, []);
 
     return (
-        <div style={{display: 'flex', justifyContent: 'center',}}>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center',}}>
             { modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} /> }
-            <FeedPhotos setModalPhoto={setModalPhoto} />
+            {pages.map(item => {
+                return <FeedPhotos key={item} user={user} page={item} setModalPhoto={setModalPhoto} setInfinite={setInfinite} />;
+            })}
         </div>
     );
 };
--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -19,16 +19,20 @@ const PhotosList = styled.ul`
     }
 `;
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
     const { data, loading, error, request } = useFetch();
+
     React.useEffect(() => {
         async function fetchPhotos() {
-            const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+            const total = 3;
+            const { url, options } = PHOTOS_GET({ page, total, user  });
             const { response, json } = await request(url, options);
-            console.log(json);
+            if( response && response.ok && json.length < total ) {
+                setInfinite(false);
+            }
         }
         fetchPhotos();
-    }, [request])
+    }, [request, user, page, setInfinite])
 
     if( error ) {
         return <Error error={error} />
